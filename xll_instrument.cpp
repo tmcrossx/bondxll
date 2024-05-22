6a41163c01c241308229da7cfc4070c36391d141
Keith A. Lewis <kal@kalx.net>
2024-05-22 06:21:04 -0400

    xll use iterable directly

--- xll_instrument.cpp
+++ xll_instrument.cpp
@@ -6,11 +6,6 @@ using namespace tmx;
 //using namespace tmx::instrument;
 using namespace xll;
 
-inline auto vector(const _FP12* p)
-{
-	return fms::iterable::vector(size(*p), array(*p));
-}
-
 AddIn xai_instrument_(
 	Function(XLL_HANDLEX, "xll_instrument_", "\\" CATEGORY ".INSTRUMENT")
 	.Arguments({
@@ -30,7 +25,9 @@ HANDLEX WINAPI xll_instrument_(const _FP12* pu, const _FP12* pc)
 		return INVALID_HANDLEX;
 	}
 
-	handle<instrument::interface<>> h_(new instrument::iterable(vector(pu), vector(pc)));
+	using namespace fms::iterable;
+
+	handle<instrument::interface<>> h_(new instrument::iterable(vector(size(*pu), array(*pu)), vector(size(*pc), array(*pc))));
 
 	return h_ ? h_.get() : INVALID_HANDLEX;
 }
