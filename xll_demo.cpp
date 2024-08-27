ba72520d8faf53fb2c0560eec2e7e48aef00af66
Keith A. Lewis <kal@kalx.net>
2024-08-26 20:08:02 -0400

    demo

---
+++ xll_demo.cpp
@@ -0,0 +1,51 @@
+// xll_demo.cpp = Demonstrations
+#include "bondxll.h"
+#include "xll24/include/macrofun.h"
+#include "xll24/include/type.h"
+
+void DoEvents(int ms);
+
+using namespace xll;
+
+//AddIn xai_instrument(Macro(L"type_instrument", L"TYPE.INSTRUMENT"));
+AddIn xai_type_instrument(Macro(L"type_instrument", L"TI"));
+int WINAPI type_instrument()
+{
+#pragma XLLEXPORT
+	try {
+		int pause = 100, wait = 1000;
+		Select sel;
+		Type("Ctrl-Shift-A", pause, wait);
+		//Excel(xlcColumnWidth, sel.selection, 3); // autofit
+		sel.Down();
+		Type("=\\TMX.INSTRUMENT", pause, 500);
+		Excel(xlSet, sel.selection, Nil);
+		Type("=\\TMX.INSTRUMENT(time, cash)", 0, wait);
+		
+		sel.Offset(-1, 1);
+		Type("Ctrl-Shift-B", pause, wait);
+		sel.Down();
+		Type("=\\TMX.INSTRUMENT", pause, wait);
+		Excel(xlcRun, L"XLL.PASTEB");
+		DoEvents(wait);	
+		sel.Down();
+		Type("=TMX.INSTRUMENT(R[-1]C[0])", pause, wait);
+		sel.Offset(-1, 0);
+		DoEvents(wait);
+
+		sel.Offset(-1, 3);
+		Type("Ctrl-Shift-C", pause, wait);
+		sel.Down();
+		Type("=\\TMX.INSTRUMENT", pause, wait);
+		Excel(xlcRun, L"XLL.PASTEC");
+		sel.Offset(3, 0);
+		Type("=TMX.INSTRUMENT(R[-3]C[0])", pause, wait);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+
+		return FALSE;
+	}
+
+	return TRUE;
+}
