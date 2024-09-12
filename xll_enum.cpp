290caba2509e8a2a1f5542a0bdec6a56bead54aa
Keith A. Lewis <kal@kalx.net>
2024-09-12 15:04:34 -0400

    pass numbers through in enum

--- xll_enum.cpp
+++ xll_enum.cpp
@@ -10,18 +10,23 @@ using namespace xll;
 AddIn xai_enum(
 	Function(XLL_LPOPER, "xll_enum", "ENUM")
 	.Arguments({
-		Arg(XLL_CSTRING, "enum", "is the string representation of an enumeration.")
+		Arg(XLL_LPOPER, "enum", "is the string representation of an enumeration.")
 		})
 	.Category(CATEGORY)
 	.FunctionHelp("Evaluate a string name of an enumeration.")
 );
-const XLOPER12* WINAPI xll_enum(const XCHAR* e)
+const XLOPER12* WINAPI xll_enum(const LPOPER e)
 {
 #pragma XLLEXPORT
 	static OPER o, l(L"="), r(L"()");
 
 	try {
-		o = Excel(xlfEvaluate, l & OPER(e) & r);
+		if (isStr(*e)) {
+			o = Excel(xlfEvaluate, l & OPER(*e) & r);
+		}
+		else {
+			o = *e;
+		}
 	}
 	catch (const std::exception& ex) {
 		XLL_ERROR(ex.what());
